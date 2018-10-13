68181d8a478d29c85c79799e1b03e6e23291057c
Tanawit Pattanaveerangkoon <tanawit.pattana@outlook.com>
2018-10-13 13:56:16 +0700

    Section 3, Lecture 26

--- cards/main.go
+++ cards/main.go
@@ -4,8 +4,6 @@ import "fmt"
 
 func main() {
 	cards := newDeck()
+	cards.saveToFile("my_cards")
 	fmt.Println(cards.toString())
-	// hand, remainingCards := deal(cards, 5)
-	// hand.print()
-	// remainingCards.print()
 }
